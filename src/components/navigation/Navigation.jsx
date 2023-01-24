9d5ca0feabfb7fc5f58cf891ba08dc525cc19fc7
selma <selma.om1@gmail.com>
2023-01-24 20:54:27 +0100

    initialized netflix-clone project

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Navigation.module.css';
+
+const Navigation = () => {
+
+return (
+    <div className={styles.navigation}>
+        <p>
+            Navigation
+        </p>
+    </div>
+)
+}
+
+export default Navigation;
