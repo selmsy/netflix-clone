3cd5a6cc40f8e65e20461aba05b2c12bad3b9438
selma <selma.om1@gmail.com>
2023-01-28 11:33:03 +0100

    added Segment component

--- src/components/titles/Titles.jsx
+++ src/components/titles/Titles.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Titles.module.css'
+import Segment from '../Segment/Segment'
 const Titles = () => {
   return (
     <div className={styles.titles}>
-        
+          <Segment title="Popular on Netflix" />
+            <Segment title="Continue watching for Home" />
+            <Segment title="Trending Now" />
+            <Segment title="Watch It Again" />
         </div>
   )
 }
